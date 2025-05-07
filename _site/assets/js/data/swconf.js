4a56a17546b03b2fc97f0314900675193a3ae7e3
Richard Chamberlain <info@sebostechnology.com>
2025-05-07 06:28:09 -0600

    add article on Suricata

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1746121953',resources: [
+    cacheName: 'chirpy-1746122211',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
