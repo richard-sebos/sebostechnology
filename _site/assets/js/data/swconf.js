c8a3edac90450cc3c88cfeab57e1f9e19748ee80
Richard Chamberlain <info@sebostechnology.com>
2025-05-19 09:26:59 -0600

    Add better SEO to ROS2 and Auditd

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1747666861',resources: [
+    cacheName: 'chirpy-1747668051',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
