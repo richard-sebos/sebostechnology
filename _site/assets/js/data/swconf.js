d31473bedc6c1d12ada4e689290191b189c098f1
Richard Chamberlain <info@sebostechnology.com>
2025-08-25 18:32:02 -0600

    Correcting error in article

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1756044302',resources: [
+    cacheName: 'chirpy-1756168139',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
