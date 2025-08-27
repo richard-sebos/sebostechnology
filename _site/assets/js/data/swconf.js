8e3240a3a9531fa174bfbd3076d654ad037b1416
Richard Chamberlain <info@sebostechnology.com>
2025-08-26 20:18:54 -0600

    Fixing image

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1756260822',resources: [
+    cacheName: 'chirpy-1756261120',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
