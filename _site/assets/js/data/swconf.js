2d643dd424312aeadcd784f182a57a22db4d8348
Richard Chamberlain <info@sebostechnology.com>
2025-09-28 15:50:57 -0600

    Weekly article

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1758476401',resources: [
+    cacheName: 'chirpy-1759092717',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
